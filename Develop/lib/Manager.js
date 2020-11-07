178b30ef81cfe088563c72aeea6afbce312be84f
Anu Rav <anusharavva@gmail.com>
2020-11-06 16:24:38 -0800

    Added Constructor classes for all JS files

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Manager{
+
+    constructor(name,id,email,officenumber){
+       super(name,id,email);
+       this.officenumber = officenumber;
+    }
+
+    getRole(){
+        return "Manager";
+    };
+
+    getOffice(){
+        return this.officenumber;
+    }
+}
+
+modules.export = Manager;
